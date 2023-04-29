9d1d3168dacb5610693666d6e1cfb2d8bca88ac3
Smoren <ofigate@gmail.com>
2023-04-29 12:43:52 +0300

    Value factory updated.

--- src/Factories/Value.php
+++ src/Factories/Value.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Smoren\Validator\Factories;
 
 use Smoren\Validator\Interfaces\BaseRuleInterface;
+use Smoren\Validator\Interfaces\CompositeRuleInterface;
 use Smoren\Validator\Interfaces\ContainerRuleInterface;
 use Smoren\Validator\Interfaces\FloatRuleInterface;
 use Smoren\Validator\Interfaces\IntegerRuleInterface;
+use Smoren\Validator\Rules\AndRule;
 use Smoren\Validator\Rules\ContainerRule;
 use Smoren\Validator\Rules\FloatRule;
 use Smoren\Validator\Rules\IntegerRule;
@@ -50,10 +52,21 @@ class Value
 
     /**
      * @param array<BaseRuleInterface> $rules
-     * @return OrRule
+     *
+     * @return CompositeRuleInterface
      */
-    public static function or(array $rules): OrRule
+    public static function or(array $rules): CompositeRuleInterface
     {
         return new OrRule($rules);
+    }
+
+    /**
+     * @param array<BaseRuleInterface> $rules
+     *
+     * @return CompositeRuleInterface
+     */
+    public static function and(array $rules): CompositeRuleInterface
+    {
+        return new AndRule($rules);
     }
 }
