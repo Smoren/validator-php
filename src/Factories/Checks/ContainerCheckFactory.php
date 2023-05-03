aa910212289c1d56bfefe282ea8a34a0a1cd4fee
Smoren <ofigate@gmail.com>
2023-05-03 10:48:33 +0300

    ContainerCheckFactory added.

---
+++ src/Factories/Checks/ContainerCheckFactory.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Smoren\Validator\Factories\Checks;
+
+use Smoren\Validator\Factories\CheckBuilder;
+use Smoren\Validator\Helpers\ContainerAccessHelper;
+use Smoren\Validator\Helpers\TypeHelper;
+use Smoren\Validator\Interfaces\CheckInterface;
+use Smoren\Validator\Interfaces\IntegerRuleInterface;
+use Smoren\Validator\Interfaces\MixedRuleInterface;
+use Smoren\Validator\Structs\CheckName;
+use Smoren\Validator\Structs\Param;
+
+class ContainerCheckFactory
+{
+    public static function getNumericCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::CONTAINER)
+            ->withPredicate(fn ($value) => \is_array($value) || \is_object($value))
+            ->build();
+    }
+
+    public static function getArrayCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::ARRAY)
+            ->withPredicate(fn ($value) => \is_array($value))
+            ->build();
+    }
+
+    public static function getIndexedArrayCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::INDEXED_ARRAY)
+            ->withPredicate(fn ($value) => \array_values($value) === $value)
+            ->withDependOnChecks([ContainerCheckFactory::getArrayCheck()])
+            ->build();
+    }
+
+    public static function getAssociativeArrayCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::ASSOCIATIVE_ARRAY)
+            ->withPredicate(fn ($value) => \array_values($value) !== $value)
+            ->withDependOnChecks([ContainerCheckFactory::getArrayCheck()])
+            ->build();
+    }
+
+    public static function getArrayAccessibleCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::ARRAY_ACCESSIBLE)
+            ->withPredicate(fn ($value) => \is_array($value) || $value instanceof \ArrayAccess)
+            ->build();
+    }
+
+    public static function getObjectCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::OBJECT)
+            ->withPredicate(fn ($value) => \is_object($value))
+            ->build();
+    }
+
+    public static function getStdObjectCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::STD_OBJECT)
+            ->withPredicate(fn ($value) => $value instanceof \stdClass)
+            ->build();
+    }
+
+    public static function getInstanceOfCheck(string $class): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::INSTANCE_OF)
+            ->withPredicate(fn ($value) => $value instanceof $class)
+            ->withCalculatedParams([Param::GIVEN_TYPE => fn ($value) => TypeHelper::getType($value)])
+            ->build();
+    }
+
+    public static function getCountableCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::COUNTABLE)
+            ->withPredicate(fn ($value) => \is_countable($value))
+            ->build();
+    }
+
+    public static function getIterableCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::ITERABLE)
+            ->withPredicate(fn ($value) => \is_iterable($value))
+            ->build();
+    }
+
+    public static function getEmptyCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::EMPTY)
+            ->withPredicate(fn ($value) => \count($value) === 0)
+            ->withDependOnChecks([ContainerCheckFactory::getCountableCheck()])
+            ->build();
+    }
+
+    public static function getNotEmptyCheck(): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::NOT_EMPTY)
+            ->withPredicate(fn ($value) => \count($value) > 0)
+            ->withDependOnChecks([ContainerCheckFactory::getCountableCheck()])
+            ->build();
+    }
+
+    public static function getLengthIsCheck(IntegerRuleInterface $rule): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::LENGTH_IS)
+            ->withPredicate(static function ($value) use ($rule) {
+                /** @var \Countable $value */
+                $rule->validate(\count($value));
+                return true;
+            })
+            ->withDependOnChecks([ContainerCheckFactory::getCountableCheck()])
+            ->build();
+    }
+
+    public static function getHasAttributeCheck(string $name): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::HAS_ATTRIBUTE)
+            ->withPredicate(fn ($value) => ContainerAccessHelper::hasAccessibleAttribute($value, $name))
+            ->withParams([Param::ATTRIBUTE => $name])
+            ->build();
+    }
+
+    public static function getHasOptionalAttributeCheck(string $name, MixedRuleInterface $rule): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::HAS_ATTRIBUTE)
+            ->withPredicate(static function ($value) use ($name, $rule) {
+                if (!ContainerAccessHelper::hasAccessibleAttribute($value, $name)) {
+                    return true;
+                }
+                $rule->validate(ContainerAccessHelper::getAttributeValue($value, $name));
+                return true;
+            })
+            ->withParams([Param::ATTRIBUTE => $name])
+            ->build();
+    }
+
+    public static function getAttributeIsCheck(string $name, MixedRuleInterface $rule): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::ATTRIBUTE_IS)
+            ->withPredicate(static function ($value, string $name) use ($rule) {
+                $rule->validate(ContainerAccessHelper::getAttributeValue($value, $name));
+                return true;
+            })
+            ->withParams([Param::ATTRIBUTE => $name])
+            ->withDependOnChecks([ContainerCheckFactory::getHasAttributeCheck($name)])
+            ->build();
+    }
+
+    public static function getAllKeysAreCheck(MixedRuleInterface $rule): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::ALL_KEYS_ARE)
+            ->withPredicate(static function ($value) use ($rule) {
+                foreach ($value as $k => $v) {
+                    $rule->validate($k);
+                }
+                return true;
+            })
+            ->withDependOnChecks([ContainerCheckFactory::getIterableCheck()])
+            ->build();
+    }
+
+    public static function getAllValuesAreCheck(MixedRuleInterface $rule): CheckInterface
+    {
+        return CheckBuilder::create(CheckName::ALL_VALUES_ARE)
+            ->withPredicate(static function ($value) use ($rule) {
+                foreach ($value as $v) {
+                    $rule->validate($v);
+                }
+                return true;
+            })
+            ->withDependOnChecks([ContainerCheckFactory::getIterableCheck()])
+            ->build();
+    }
+}
--- src/Rules/ContainerRule.php
+++ src/Rules/ContainerRule.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Smoren\Validator\Rules;
 
 use Smoren\Validator\Factories\CheckBuilder;
+use Smoren\Validator\Factories\Checks\ContainerCheckFactory;
 use Smoren\Validator\Helpers\ContainerAccessHelper;
 use Smoren\Validator\Helpers\TypeHelper;
 use Smoren\Validator\Interfaces\MixedRuleInterface;
-use Smoren\Validator\Interfaces\CheckInterface;
 use Smoren\Validator\Interfaces\ContainerRuleInterface;
 use Smoren\Validator\Interfaces\IntegerRuleInterface;
 use Smoren\Validator\Structs\CheckName;
@@ -22,12 +22,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
     public function __construct(string $name)
     {
         parent::__construct($name);
-        $this->check(
-            CheckBuilder::create(CheckName::CONTAINER)
-                ->withPredicate(fn ($value) => \is_array($value) || \is_object($value))
-                ->build(),
-            true
-        );
+        $this->check(ContainerCheckFactory::getNumericCheck(), true);
     }
 
     /**
@@ -37,7 +32,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function array(bool $stopOnViolation = true): self
     {
-        return $this->check($this->getArrayCheck(), $stopOnViolation);
+        return $this->check(ContainerCheckFactory::getArrayCheck(), $stopOnViolation);
     }
 
     /**
@@ -47,13 +42,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function indexedArray(bool $stopOnViolation = true): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::INDEXED_ARRAY)
-                ->withPredicate(fn ($value) => \array_values($value) === $value)
-                ->withDependOnChecks([$this->getArrayCheck()])
-                ->build(),
-            $stopOnViolation
-        );
+        return $this->check(ContainerCheckFactory::getIndexedArrayCheck(), $stopOnViolation);
     }
 
     /**
@@ -63,13 +52,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function associativeArray(bool $stopOnViolation = true): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::ASSOCIATIVE_ARRAY)
-                ->withPredicate(fn ($value) => \array_values($value) !== $value)
-                ->withDependOnChecks([$this->getArrayCheck()])
-                ->build(),
-            $stopOnViolation
-        );
+        return $this->check(ContainerCheckFactory::getAssociativeArrayCheck(), $stopOnViolation);
     }
 
     /**
@@ -79,12 +62,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function arrayAccessible(bool $stopOnViolation = true): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::ARRAY_ACCESSIBLE)
-                ->withPredicate(fn ($value) => \is_array($value) || $value instanceof \ArrayAccess)
-                ->build(),
-            $stopOnViolation
-        );
+        return $this->check(ContainerCheckFactory::getArrayAccessibleCheck(), $stopOnViolation);
     }
 
     /**
@@ -94,7 +72,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function iterable(bool $stopOnViolation = true): self
     {
-        return $this->check($this->getIterableCheck(), $stopOnViolation);
+        return $this->check(ContainerCheckFactory::getIterableCheck(), $stopOnViolation);
     }
 
     /**
@@ -104,7 +82,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function countable(bool $stopOnViolation = true): self
     {
-        return $this->check($this->getCountableCheck(), $stopOnViolation);
+        return $this->check(ContainerCheckFactory::getCountableCheck(), $stopOnViolation);
     }
 
     /**
@@ -114,12 +92,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function object(bool $stopOnViolation = true): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::OBJECT)
-                ->withPredicate(fn ($value) => \is_object($value))
-                ->build(),
-            $stopOnViolation
-        );
+        return $this->check(ContainerCheckFactory::getObjectCheck(), $stopOnViolation);
     }
 
     /**
@@ -129,12 +102,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function stdObject(bool $stopOnViolation = true): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::STD_OBJECT)
-                ->withPredicate(fn ($value) => $value instanceof \stdClass)
-                ->build(),
-            $stopOnViolation
-        );
+        return $this->check(ContainerCheckFactory::getStdObjectCheck(), $stopOnViolation);
     }
 
     /**
@@ -144,13 +112,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function instanceOf(string $class, bool $stopOnViolation = true): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::INSTANCE_OF)
-                ->withPredicate(fn ($value) => $value instanceof $class)
-                ->withCalculatedParams([Param::GIVEN_TYPE => fn ($value) => TypeHelper::getType($value)])
-                ->build(),
-            $stopOnViolation
-        );
+        return $this->check(ContainerCheckFactory::getInstanceOfCheck($class), $stopOnViolation);
     }
 
     /**
@@ -160,12 +122,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function empty(): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::EMPTY)
-                ->withPredicate(fn ($value) => \count($value) === 0)
-                ->withDependOnChecks([$this->getCountableCheck()])
-                ->build()
-        );
+        return $this->check(ContainerCheckFactory::getEmptyCheck());
     }
 
     /**
@@ -175,12 +132,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function notEmpty(): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::NOT_EMPTY)
-                ->withPredicate(fn ($value) => \count($value) > 0)
-                ->withDependOnChecks([$this->getCountableCheck()])
-                ->build()
-        );
+        return $this->check(ContainerCheckFactory::getNotEmptyCheck());
     }
 
     /**
@@ -190,16 +142,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function lengthIs(IntegerRuleInterface $rule): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::LENGTH_IS)
-                ->withPredicate(static function ($value) use ($rule) {
-                    /** @var \Countable $value */
-                    $rule->validate(\count($value));
-                    return true;
-                })
-                ->withDependOnChecks([$this->getCountableCheck()])
-                ->build()
-        );
+        return $this->check(ContainerCheckFactory::getLengthIsCheck($rule));
     }
 
     /**
@@ -210,19 +153,10 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
     public function hasAttribute(string $name, ?MixedRuleInterface $rule = null): self
     {
         if ($rule === null) {
-            return $this->check($this->getHasAttributeCheck($name));
+            return $this->check(ContainerCheckFactory::getHasAttributeCheck($name));
         }
 
-        return $this->check(
-            CheckBuilder::create(CheckName::ATTRIBUTE_IS)
-                ->withPredicate(static function ($value, string $name) use ($rule) {
-                    $rule->validate(ContainerAccessHelper::getAttributeValue($value, $name));
-                    return true;
-                })
-                ->withParams([Param::ATTRIBUTE => $name])
-                ->withDependOnChecks([$this->getHasAttributeCheck($name)])
-                ->build()
-        );
+        return $this->check(ContainerCheckFactory::getAttributeIsCheck($name, $rule));
     }
 
     /**
@@ -232,18 +166,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function hasOptionalAttribute(string $name, MixedRuleInterface $rule): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::HAS_ATTRIBUTE)
-                ->withPredicate(static function ($value) use ($name, $rule) {
-                    if (!ContainerAccessHelper::hasAccessibleAttribute($value, $name)) {
-                        return true;
-                    }
-                    $rule->validate(ContainerAccessHelper::getAttributeValue($value, $name));
-                    return true;
-                })
-                ->withParams([Param::ATTRIBUTE => $name])
-                ->build()
-        );
+        return $this->check(ContainerCheckFactory::getHasOptionalAttributeCheck($name, $rule));
     }
 
     /**
@@ -253,17 +176,7 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function allKeysAre(MixedRuleInterface $rule): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::ALL_KEYS_ARE)
-                ->withPredicate(static function ($value) use ($rule) {
-                    foreach ($value as $k => $v) {
-                        $rule->validate($k);
-                    }
-                    return true;
-                })
-                ->withDependOnChecks([$this->getIterableCheck()])
-                ->build()
-        );
+        return $this->check(ContainerCheckFactory::getAllKeysAreCheck($rule));
     }
 
     /**
@@ -273,45 +186,6 @@ class ContainerRule extends MixedRule implements ContainerRuleInterface
      */
     public function allValuesAre(MixedRuleInterface $rule): self
     {
-        return $this->check(
-            CheckBuilder::create(CheckName::ALL_VALUES_ARE)
-                ->withPredicate(static function ($value) use ($rule) {
-                    foreach ($value as $v) {
-                        $rule->validate($v);
-                    }
-                    return true;
-                })
-                ->withDependOnChecks([$this->getIterableCheck()])
-                ->build()
-        );
-    }
-
-    protected function getArrayCheck(): CheckInterface
-    {
-        return CheckBuilder::create(CheckName::ARRAY)
-            ->withPredicate(fn ($value) => \is_array($value))
-            ->build();
-    }
-
-    protected function getCountableCheck(): CheckInterface
-    {
-        return CheckBuilder::create(CheckName::COUNTABLE)
-            ->withPredicate(fn ($value) => \is_countable($value))
-            ->build();
-    }
-
-    protected function getIterableCheck(): CheckInterface
-    {
-        return CheckBuilder::create(CheckName::ITERABLE)
-            ->withPredicate(fn ($value) => \is_iterable($value))
-            ->build();
-    }
-
-    protected function getHasAttributeCheck(string $name): CheckInterface
-    {
-        return CheckBuilder::create(CheckName::HAS_ATTRIBUTE)
-            ->withPredicate(fn ($value) => ContainerAccessHelper::hasAccessibleAttribute($value, $name))
-            ->withParams([Param::ATTRIBUTE => $name])
-            ->build();
+        return $this->check(ContainerCheckFactory::getAllValuesAreCheck($rule));
     }
 }
